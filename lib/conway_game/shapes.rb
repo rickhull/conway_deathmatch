80f104490f3ec9a964482fae4d9e604ac9b61157
Rick Hull <rick.hull@gmail.com>
2014-11-24 00:43:59 -0500

    shapes refactor

--- lib/conway_game/shapes.rb
+++ lib/conway_game/shapes.rb
@@ -1,21 +1,22 @@
 require 'conway_game'
 require 'yaml'
 
-class ConwayGame::BoardState
-  module Extras
-    @@known_shapes = nil
+module ConwayGame
+  module Shapes
+    @@known
 
     # memoize lib/conway_game/data/shapes.yaml
-    def known_shapes
-      @@known_shapes ||=
-        YAML.load_file(File.join(__dir__, 'data', 'shapes.yaml'))
+    def self.known
+      @@known ||= YAML.load_file(File.join(__dir__, 'data', 'shapes.yaml'))
     end
 
     # parse a string like "acorn 12 22 block 5 0 p 1 2 p 3 4 p 56 78"
     # add known shapes
-    def add_shapes(str, val = ALIVE)
+    def self.add(board, str, val = BoardState::ALIVE)
       tokens = str.split
       points = []
+      known = self.known
+      
       while !tokens.empty?
         shape = tokens.shift.downcase
         raise "no coordinates for #{shape}" if tokens.length < 2
@@ -25,14 +26,10 @@ class ConwayGame::BoardState
         when 'p'
           points << [x, y]
         else
-          if (known = self.known_shapes[shape])
-            self.add_points(known, x, y, val)
-          else
-            raise "unknown shape: #{shape}"
-          end
+          board.add_points(known.fetch(shape), x, y, val)
         end
       end
-      self.add_points(points, 0, 0, val)
+      board.add_points(points, 0, 0, val)
     end
   end
 end
