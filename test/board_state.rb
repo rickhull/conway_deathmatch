3161df1197d87dd7638932d7f7065cd991fde2aa
Rick Hull <rick.hull@gmail.com>
2014-12-03 04:33:01 -0500

    add simple benchmarks

--- test/board_state.rb
+++ test/board_state.rb
@@ -67,3 +67,29 @@ describe ConwayDeathmatch::BoardState do
     end
   end
 end
+
+if ENV['BENCH']
+  require 'minitest/benchmark'
+
+  describe "Create table" do
+    bench_range do
+      bench_exp 9, 9999, 3
+    end
+
+    bench_performance_linear "width*height", 0.9 do |n|
+      BoardState.new(n, n)
+    end
+  end
+
+  describe "Board ticks" do
+    bench_range do
+      bench_exp 1, 100, 3
+    end
+
+    bench_performance_linear "acorn demo", 0.9995 do |n|
+      b = BoardState.new(70, 40)
+      Shapes.add(b, "acorn 50 18")
+      n.times { b.tick }
+    end
+  end
+end
