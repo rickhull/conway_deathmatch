aea788227d92afd62447e5789b163ce5af40091a
Rick Hull <rick.hull@gmail.com>
2014-12-03 23:20:05 -0500

    accept BENCH_\*_THRESH env vars

--- test/bench_board_state.rb
+++ test/bench_board_state.rb
@@ -5,12 +5,15 @@ require 'conway_deathmatch'
 
 include ConwayDeathmatch
 
+BENCH_NEW_THRESH = ENV['BENCH_NEW_THRESH'] || 0.9
+BENCH_TICK_THRESH = ENV['BENCH_TICK_THRESH'] || 0.9995
+
 describe "BoardState.new Benchmark" do
   bench_range do
     bench_exp 9, 9999, 3
   end
 
-  bench_performance_linear "width*height", 0.9 do |n|
+  bench_performance_linear "width*height", BENCH_NEW_THRESH do |n|
     BoardState.new(n, n)
   end
 end
@@ -20,7 +23,7 @@ describe "BoardState#tick Benchmark" do
     bench_exp 1, 100, 3
   end
 
-  bench_performance_linear "acorn demo", 0.9995 do |n|
+  bench_performance_linear "acorn demo", BENCH_TICK_THRESH do |n|
     b = BoardState.new(70, 40)
     Shapes.add(b, "acorn 50 18")
     n.times { b.tick }
