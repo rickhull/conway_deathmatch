ad10fbb59d76d35662c376139a8db11053675613
Rick Hull <rick.hull@gmail.com>
2015-01-16 12:20:44 -0800

    separate common project specs from env-specific
    - prod: consolidate_protocols
    - perf: consolidate_protocols, elixirc_paths
    - dev, test: nothing specific

--- mix.exs
+++ mix.exs
@@ -2,14 +2,12 @@ defmodule ConwayDeathmatch.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :conway_deathmatch,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     escript: [main_module: ConwayDeathmatch.CLI],
-     consolidate_protocols: true,
-     elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps,
-    ]
+    common = [app: :conway_deathmatch,
+              version: "0.0.1",
+              elixir: "~> 1.0",
+              escript: [main_module: ConwayDeathmatch.CLI],
+              deps: deps]
+    Keyword.merge(common, project(Mix.env))
   end
 
   # Configuration for the OTP application
@@ -19,9 +17,10 @@ defmodule ConwayDeathmatch.Mixfile do
     [applications: [:logger]]
   end
 
-  # only compile perf stuff for perf environment
-  defp elixirc_paths(:perf), do: ["lib", "perf"]
-  defp elixirc_paths(env) when env in [:dev, :test, :prod], do: ["lib"]
+  defp project(:prod), do: [consolidate_protocols: true]
+  defp project(:perf), do: [consolidate_protocols: true,
+                            elixirc_paths: ["lib", "perf"]]
+  defp project(env) when env in [:dev, :test], do: []
 
   defp deps, do: [exprof, eflame]
 
