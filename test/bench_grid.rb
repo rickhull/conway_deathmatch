75e13404dc89c338cb1c8f0a2ac34c50a8124169
Rick Hull <rick.hull@gmail.com>
2017-10-11 18:10:54 +0000

    minor reformat

--- test/bench_grid.rb
+++ test/bench_grid.rb
@@ -3,8 +3,8 @@ require 'minitest/benchmark'
 require 'conway_deathmatch'
 require 'conway_deathmatch/shapes'
 
-BENCH_NEW_THRESH = (ENV['BENCH_NEW_THRESH'] || 0.9).to_f
-BENCH_TICK_THRESH = (ENV['BENCH_TICK_THRESH'] || 0.9995).to_f
+bnew = (ENV['BENCH_NEW_THRESH'] || 0.9).to_f
+btick = (ENV['BENCH_TICK_THRESH'] || 0.9995).to_f
 Shapes = ConwayDeathmatch::Shapes
 
 describe "ConwayDeathmatch.new Benchmark" do
@@ -12,7 +12,7 @@ describe "ConwayDeathmatch.new Benchmark" do
     bench_exp 9, 9999, 3
   end
 
-  bench_performance_linear "width*height", BENCH_NEW_THRESH do |n|
+  bench_performance_linear "width*height", bnew do |n|
     ConwayDeathmatch.new(n, n)
   end
 end
@@ -22,14 +22,13 @@ describe "ConwayDeathmatch#tick Benchmark" do
     bench_exp 1, 100, 3
   end
 
-  bench_performance_linear "acorn demo", BENCH_TICK_THRESH do |n|
+  bench_performance_linear "acorn demo", btick do |n|
     b = ConwayDeathmatch.new(70, 40)
     Shapes.add(b, "acorn 50 18")
     n.times { b.tick }
   end
 
-  bench_performance_linear "aggressive deathmatch demo",
-                           BENCH_TICK_THRESH do |n|
+  bench_performance_linear "aggressive deathmatch demo", btick do |n|
     b = ConwayDeathmatch.new(70, 40)
     b.deathmatch = :aggressive
     Shapes.add(b, "acorn 30 30", "1")
@@ -37,8 +36,7 @@ describe "ConwayDeathmatch#tick Benchmark" do
     n.times { b.tick }
   end
 
-  bench_performance_linear "defensive deathmatch demo",
-                           BENCH_TICK_THRESH do |n|
+  bench_performance_linear "defensive deathmatch demo", btick do |n|
     b = ConwayDeathmatch.new(70, 40)
     b.deathmatch = :defensive
     Shapes.add(b, "acorn 30 30", "1")
@@ -46,8 +44,7 @@ describe "ConwayDeathmatch#tick Benchmark" do
     n.times { b.tick }
   end
 
-  bench_performance_linear "friendly deathmatch demo",
-                           BENCH_TICK_THRESH do |n|
+  bench_performance_linear "friendly deathmatch demo", btick do |n|
     b = ConwayDeathmatch.new(70, 40)
     b.deathmatch = :friendly
     Shapes.add(b, "acorn 30 30", "1")
