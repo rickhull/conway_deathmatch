9faaa33f0fcc6e2ecd31dfd74ead9e9648a2e43d
Rick Hull <rick.hull@gmail.com>
2022-05-02 15:17:34 -0400

    support CODE_COVERAGE=false; drop the HTML stuff

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,11 @@
-if ENV['CODE_COVERAGE']
+if ENV['CODE_COVERAGE'] and
+  !%w[false no].include?(ENV['CODE_COVERAGE'].downcase)
   require 'simplecov'
+  require "simplecov_json_formatter"
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::JSONFormatter,
+    SimpleCov::Formatter::SimpleFormatter,
+  ]
   SimpleCov.start
 end
-
 require 'minitest/autorun'
